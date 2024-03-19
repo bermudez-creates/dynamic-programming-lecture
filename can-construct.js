a7a699d41852bd3f371cf2a839c99f40910a5e06
Steezy <bermudezalejandro45@gmail.com>
2024-03-19 13:23:43 -0400

    Can construct problem - Memoized

--- can-construct.js
+++ can-construct.js
@@ -5,7 +5,7 @@
 
 // You may reuse the elements of the `wordBank` array
 
-const canConstruct = (target, wordBank) => {
+const Construct = (target, wordBank) => {
   if (target === '') {
     return true;
   }
@@ -26,6 +26,58 @@ const canConstruct = (target, wordBank) => {
   return false;
 };
 
+// BRUTE FORCE APPROACH
+// # TIME COMLEXITY
+// m: target length
+// n: nodes (array/ length of arr)
+// O(n^m * m) => it is n^m because for every node (worst case) we multiply it by itself until we reach the target
+// if every node had a matching word we'd have to go through every node m times
+// it is * m because of the copying factor .slice()
+
+// # SPACE COMPLECITY
+// O(m^2)
+
+// console.log(Construct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']));
+
+// console.log(
+//   Construct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])
+// );
+
+// console.log(
+//   Construct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't'])
+// );
+
+// console.log(
+//   Construct('eeeeeeeeeeeeeeeef', [
+//     'e',
+//     'ee',
+//     'eee',
+//     'eeee',
+//     'eeeee',
+//     'eeeeeee',
+//   ])
+// );
+
+const canConstruct = (target, wordBank, memo = {}) => {
+  if (target in memo) return memo[target];
+
+  if (target === '') return true;
+
+  for (word of wordBank) {
+    if (target.indexOf(word) === 0) {
+      const suffix = target.slice(word.length);
+
+      if (canConstruct(suffix, wordBank, memo) === true) {
+        memo[target] = true;
+        return true;
+      }
+    }
+  }
+
+  memo[target] = false;
+  return false;
+};
+
 console.log(canConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']));
 
 console.log(
@@ -36,13 +88,23 @@ console.log(
   canConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't'])
 );
 
-// console.log(
-//   canConstruct('eeeeeeeeeeeeeeeef', [
-//     'e',
-//     'ee',
-//     'eee',
-//     'eeee',
-//     'eeeee',
-//     'eeeeeee',
-//   ])
-// );
+console.log(
+  canConstruct('eeeeeeeeeeeeeeeef', [
+    'e',
+    'ee',
+    'eee',
+    'eeee',
+    'eeeee',
+    'eeeeeee',
+  ])
+);
+
+// MEMOIZATION APPROACH
+
+// # TIME COMPLEXITY
+// m: target.length (height of tree)
+// n: array.length (number of nodes)
+// O(n * m^2) => with memoization we dont explore every duplicate node for subtree ... we store it instead
+
+// # SPACE COMPLEXITY
+// O(m^2) => Stays the same because of copying factor
